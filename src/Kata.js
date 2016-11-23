2deef803f8788b675ffef94bb731e9904799f812
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-11-23 22:34:46 +0100

    Last solution

--- src/Kata.js
+++ src/Kata.js
@@ -1,24 +1,38 @@
 function scramble(str1, str2) {
-    var toFind = str2.split('').sort(),
-        store = str1.split(''),
+    var toFind = str2.split('').sort(), 
+        store = str1.split('').sort(),  
         f = 0, qtyNedded, qtyInStore, 
         elem;
 
     while (f < toFind.length) {
-        elem =  toFind[f];
+        elem = toFind[f];
         qtyNedded = 1;
         while (toFind[f+qtyNedded]===elem){
             qtyNedded++;
         } 
-        f = f + qtyNedded;
-
-        qtyInStore = store.filter(function(element){
-            return elem === element;
-        }).length;
-
-        if (qtyNedded > qtyInStore){
+        
+        var g = f;
+        qtyInStore = 0;
+        while (g < store.length && qtyInStore<qtyNedded){
+            if (store[g] === elem){
+                qtyInStore++;
+            }
+            g++;
+        }
+        if (qtyInStore!==qtyNedded){
             return false;
-        }        
+        }
+
+        f = f + qtyNedded;      
     };  
     return true;
 };
+
+/*
+
+aace
+
+aabbcdef
+
+
+*/
